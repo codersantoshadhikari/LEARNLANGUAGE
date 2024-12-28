ab6716ad9d3d86728b32321135af330e61211707
SANTOSH ADHIKARI <santoshadhikari@gmail.com>
2024-12-28 08:56:36 +0545

    ADDED FULL

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
-
-
-import 'screens/HomeScreen.dart';
+import 'package:language_learning_ui/pages/choose_language.dart';
+import 'package:language_learning_ui/pages/create_account.dart';
+import 'package:language_learning_ui/pages/dashboard.dart';
+import 'package:language_learning_ui/pages/home.dart';
+import 'package:language_learning_ui/pages/lesson_screen.dart';
 
 void main() {
-  runApp(
-      const MaterialApp(
-    home: HomeScreen(),
-    debugShowCheckedModeBanner: false,
-  ),
-  );
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: () => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.rubikTextTheme(
+            TextTheme(),
+          ),
+        ),
+        initialRoute: "/",
+        onGenerateRoute: _onGenerateRoute,
+      ),
+    );
+  }
+}
 
+Route<dynamic> _onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case "/":
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return Home();
+      });
+    case "/create-account":
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return CreateAccount();
+      });
+    case "/choose-language":
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return ChooseLanguage();
+      });
+    case "/dashboard":
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return Dashboard();
+      });
+    case "/lesson-screen":
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return LessonScreen();
+      });
+    default:
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return Home();
+      });
+  }
+}
