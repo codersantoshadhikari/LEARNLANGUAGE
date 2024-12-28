76852ae8d19704915bc53459d932bdae236b763f
SANTOSH ADHIKARI <santoshadhikari@gmail.com>
2024-12-28 08:51:33 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+import 'screens/HomeScreen.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ),
+  );
+}
+
+
